bf47903747b832225e3435ecc70c330d6318f6d6
Mateus Bezerra <mateusbzerra@gmail.com>
2020-07-25 13:36:33 -0300

    setup styled global style & add basic head info

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import App from 'next/app';
+import Head from 'next/head';
+import GlobalStyle from 'styles/global';
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          <title>React Avançado - Boilerplate</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <meta
+            name="description"
+            content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+          />
+        </Head>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </>
+    );
+  }
+}
